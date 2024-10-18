7f11d2b733d057b0830f0b85c9fb44a37ed28c6e
jaebinGit <tnwoql327@gmail.com>
2024-10-18 13:01:30 +0900

    Docs : sg setting

--- eks-managed-node-group/outputs.tf
+++ eks-managed-node-group/outputs.tf
@@ -5,6 +5,14 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    description = "Allow HTTP traffic from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow HTTPS traffic from the internet"
     from_port   = 443
     to_port     = 443
@@ -13,23 +21,45 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    description = "Allow traffic to NodePort on worker nodes"
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = [module.vpc.vpc_cidr_block]
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# EKS 워커 노드 보안 그룹에 인바운드 규칙 추가
-resource "aws_security_group_rule" "allow_alb_to_nodeport" {
+# EKS 노드 보안 그룹 규칙 수정
+resource "aws_security_group_rule" "allow_alb_to_nodes" {
   type                     = "ingress"
-  from_port                = 30080  # Istio Ingress Gateway의 NodePort 번호 시작
-  to_port                  = 30080  # Istio Ingress Gateway의 NodePort 번호 끝
+  from_port                = 0
+  to_port                  = 65535
   protocol                 = "tcp"
   security_group_id        = module.eks.node_security_group_id
   source_security_group_id = aws_security_group.alb_sg.id
-  description              = "Allow ALB to access NodePort 30080"
+  description              = "Allow ALB to access all ports on nodes"
+}
+
+# Istio 관련 규칙
+resource "aws_security_group_rule" "istio" {
+  description              = "Istio"
+  type                     = "ingress"
+  from_port                = 15017
+  to_port                  = 15017
+  protocol                 = "tcp"
+  source_security_group_id = module.eks.cluster_security_group_id
+  security_group_id        = module.eks.node_security_group_id
+}
+
+# EKS 노드 간 통신을 위한 규칙
+resource "aws_security_group_rule" "allow_node_to_node" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  security_group_id        = module.eks.node_security_group_id
+  source_security_group_id = module.eks.node_security_group_id
+  description              = "Allow all traffic between nodes"
 }
 
 # Ingress 리소스에서 ALB 보안 그룹 ID를 참조하도록 변수 설정
@@ -40,14 +70,4 @@ locals {
 output "alb_security_group_id" {
   description = "The ID of the ALB security group"
   value       = aws_security_group.alb_sg.id
-}
-
-resource "aws_security_group_rule" "istio" {
-  description              = "Istio"
-  type                     = "ingress"
-  from_port                = 15017
-  to_port                  = 15017
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.cluster_security_group_id
-  security_group_id        = module.eks.node_security_group_id
 }
